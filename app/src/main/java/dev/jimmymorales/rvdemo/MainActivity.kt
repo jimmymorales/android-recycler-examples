1c445dc02e2ef615a4d2a2342c6f18e363dc1a18
Jimmy <me@jimmymorales.dev>
2021-03-17 18:57:13 -0600

    feat(home): add initial home layout

--- app/src/main/java/dev/jimmymorales/rvdemo/MainActivity.kt
+++ app/src/main/java/dev/jimmymorales/rvdemo/MainActivity.kt
@@ -1,5 +1,18 @@
 package dev.jimmymorales.rvdemo
 
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import dev.jimmymorales.rvdemo.databinding.MainFragmentBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
+
+class MainFragment : Fragment(R.layout.main_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = MainFragmentBinding.bind(view)
+
+    }
+}
